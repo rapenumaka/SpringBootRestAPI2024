d7e609c8bf35c1d36dde00bcfe6a251ce9388133
RAJA PENUMAKA <rajapenumaka@RAJAs-MacBook-Pro.local>
2024-05-07 21:29:53 -0500

    added project

---
+++ SpringBootRestAPI2024/src/main/java/com/amex/app/SpringBootRestApi2024Application.java
@@ -0,0 +1,13 @@
+package com.amex.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApi2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApi2024Application.class, args);
+	}
+
+}
---
+++ SpringBootRestAPI2024/src/test/java/com/amex/app/SpringBootRestApi2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amex.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApi2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
